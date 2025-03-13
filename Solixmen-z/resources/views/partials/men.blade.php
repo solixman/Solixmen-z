a9431fd8a5decb8484a06171aeb25c89f13728ea
soulayman <soulaymanjaa09@gmail.com>
2025-03-13 14:31:16 +0100

    template

---
+++ Solixmen-z/resources/views/partials/men.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+<h1>Maaaaaaan , this sucks!</h1>
+@endsection
--- Solixmen-z/routes/web.php
+++ Solixmen-z/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test',function (){
+    return view('partials.home');
+});
+
+
+
+Route::get('/men',function (){
+    return view('partials.men');
 });
